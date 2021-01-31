a2f1589d1c3bcacbf7f80e99b82bcc08b88ab16e
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-31 22:12:37 +0530

    dataset

---
+++ main.py
@@ -0,0 +1,21 @@
+%tensorflow_version 2.x
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compat.v2.feature_column as fc
+
+import tensorflow as tf
+
+# Load dataset.
+dftrain = pd.read_csv('https://firebasestorage.googleapis.com/v0/b/bookstore-81666.appspot.com/o/bucket%2Fdata1.csv?alt=media&token=d4a0d2f6-44d0-4a86-8212-ade80bac92fb') # training data
+dfeval = pd.read_csv('https://firebasestorage.googleapis.com/v0/b/bookstore-81666.appspot.com/o/bucket%2Fdata2.csv?alt=media&token=947a0a1c-60f2-4e26-9751-1a8e609c4905') # testing
+print(dftrain.head())
+
+y_train = dftrain.pop('paid')
+y_eval = dfeval.pop('paid')
