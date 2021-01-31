4837e0954e3b1578bb0fd8cccf13e016cc5276e9
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-31 22:14:08 +0530

    linear estimator

--- main.py
+++ main.py
@@ -28,3 +28,17 @@ dftrain['RepID'].value_counts().plot(kind='barh')
 pd.concat([dftrain, y_train], axis=1).groupby('RepID').PaymentAmount.mean().plot(kind='barh').set_xlabel('% PaymentAmount')
 
 dftrain['ICode'].value_counts().plot(kind='barh')
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
